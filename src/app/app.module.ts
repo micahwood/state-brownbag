041c5c899bbb4d21fc042166f2f6cc90e06bf532
Micah Wood <micah.wood@stgconsulting.com>
2019-06-11 23:12:58 -0600

    Add expansion panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,18 +10,9 @@ import { FavoritesComponent } from './favorites/favorites.component';
 import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BoxComponent,
-    FavoritesComponent,
-    DetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, BoxComponent, FavoritesComponent, DetailComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatExpansionModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
